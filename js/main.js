8d8ebcde35b33710ab4035e97b17aa34fc781061
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-27 18:03:53 +0300

    easing

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var tlanimation = new TimelineMax();
 
 tlanimation
-  .to(".circle", 2, { x: 100 })
-  .to(".square", 0.5, { x: 100 })
-  .to(".rectangle", 1, { x: 100 });
+  .to(".circle", 1, { x: 100, ease: Back.easeOut })
+  .to(".square", 0.5, { x: 100, ease: Elastic.easeIn })
+  .to(".rectangle", 1, { x: 100, ease: Bounce.easeIn });
