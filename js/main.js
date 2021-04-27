428049e78f41755c5dbdffb53846208ae1f00eb9
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-27 23:14:02 +0300

    Playback Controls

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 var tlshapes = new TimelineMax();
-//label fired at same time animation
-tlshapes
-  .to(".circle", 1, { x: 100 }, 5, "circleRectangle")
-  .to(".square", 1, { x: 100 }, "-=0.25")
-  .to(".rectangle", 1, { x: 100 }, "circleRectangle+=1");
+
+tlshapes.staggerFrom(".shape", 0.5, { x: 50, y: 50, opacity: 0 }, 0.2);
+
+$(".playButton").click(function () {
+  tlshapes.play();
+});
+$(".pauseButton").click(function () {
+  tlshapes.pause();
+});
+$(".resumeButton").click(function () {
+  tlshapes.resume();
+});
+$(".reverseButton").click(function () {
+  tlshapes.reverse();
+});
+$(".slowButton").click(function () {
+  tlshapes.timeScale(0.5);
+});
+$(".fastButton").click(function () {
+  tlshapes.timeScale(5);
+});
+$(".seekButton").click(function () {
+  tlshapes.seek(2);
+});
+$(".progressButton").click(function () {
+  tlshapes.progress(0.5);
+});
