38907ce959976a183b71837d5d8ad96ba709d609
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-27 21:56:54 +0300

    staggerFrom and staggerTo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-var tlanimation = new TimelineMax();
+// var tlanimation = new TimelineMax();
 
-tlanimation
-  .to(".circle", 1, { x: 100, ease: Back.easeOut })
-  .to(".square", 0.5, { x: 100, ease: Elastic.easeIn })
-  .to(".rectangle", 1, { x: 100, ease: Bounce.easeIn });
+// tlanimation
+//   .to(".circle", 1, { x: 100, ease: Back.easeOut })
+//   .to(".square", 0.5, { x: 100, ease: Elastic.easeIn })
+//   .to(".rectangle", 1, { x: 100, ease: Bounce.easeIn });
+
+TweenMax.staggerFrom(
+  ".triangle",
+  1,
+  { x: 200, y: 200, ease: Back.easeOut, opacity: 0 },
+  0.1
+);
