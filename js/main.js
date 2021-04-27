c952a7eeb7f4dc543caaa16bc59b5bf926abbc05
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-27 22:35:20 +0300

    labels and Timings

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-TweenMax.set(".oval", { x: -100 });
-
-// repeat to infinity
-// var tlanimation = new TimelineMax({ repeat: -1 });
-
-// var tlanimation = new TimelineMax({ repeat: -1, repeatDelay: 2 });
-var tlanimation = new TimelineMax({ repeat: -1, yoyo: true });
-
-tlanimation.to(".oval", 1, { x: 100 }).to("oval", 1, { x: -100 });
-
-// var tlanimation = new TimelineMax();
-
-// tlanimation
-//   .to(".circle", 1, { x: 100, ease: Back.easeOut })
-//   .to(".square", 0.5, { x: 100, ease: Elastic.easeIn })
-//   .to(".rectangle", 1, { x: 100, ease: Bounce.easeIn });
-
-// TweenMax.staggerFrom(
-//   ".triangle",
-//   1,
-//   { x: 200, y: 200, ease: Back.easeOut, opacity: 0 },
-//   0.1
-// );
+var tlshapes = new TimelineMax();
+//label fired at same time animation
+tlshapes
+  .to(".circle", 1, { x: 100 }, 5, "circleRectangle")
+  .to(".square", 1, { x: 100 }, "-=0.25")
+  .to(".rectangle", 1, { x: 100 }, "circleRectangle+=1");
