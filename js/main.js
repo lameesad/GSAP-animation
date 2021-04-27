54b2759b24339d7118e898adb9d3f5f7d8b40c2c
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-27 23:26:11 +0300

    fromTo and staggerFromTo

--- js/main.js
+++ js/main.js
@@ -1,28 +1,36 @@
-var tlshapes = new TimelineMax();
+// var tlshapes = new TimelineMax();
 
-tlshapes.staggerFrom(".shape", 0.5, { x: 50, y: 50, opacity: 0 }, 0.2);
+// tlshapes.staggerFrom(".shape", 0.5, { x: 50, y: 50, opacity: 0 }, 0.2);
 
-$(".playButton").click(function () {
-  tlshapes.play();
-});
-$(".pauseButton").click(function () {
-  tlshapes.pause();
-});
-$(".resumeButton").click(function () {
-  tlshapes.resume();
-});
-$(".reverseButton").click(function () {
-  tlshapes.reverse();
-});
-$(".slowButton").click(function () {
-  tlshapes.timeScale(0.5);
-});
-$(".fastButton").click(function () {
-  tlshapes.timeScale(5);
-});
-$(".seekButton").click(function () {
-  tlshapes.seek(2);
-});
-$(".progressButton").click(function () {
-  tlshapes.progress(0.5);
-});
+// $(".playButton").click(function () {
+//   tlshapes.play();
+// });
+// $(".pauseButton").click(function () {
+//   tlshapes.pause();
+// });
+// $(".resumeButton").click(function () {
+//   tlshapes.resume();
+// });
+// $(".reverseButton").click(function () {
+//   tlshapes.reverse();
+// });
+// $(".slowButton").click(function () {
+//   tlshapes.timeScale(0.5);
+// });
+// $(".fastButton").click(function () {
+//   tlshapes.timeScale(5);
+// });
+// $(".seekButton").click(function () {
+//   tlshapes.seek(2);
+// });
+// $(".progressButton").click(function () {
+//   tlshapes.progress(0.5);
+// });
+
+TweenMax.staggerFromTo(
+  ".triangle",
+  1,
+  { y: 200, x: 200 },
+  { y: -50, x: -50 },
+  0.2
+);
