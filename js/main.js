03b1f3a6c3dee9e5648ae2d2a08408349d707f39
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-04-27 22:11:06 +0300

    special properties

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+TweenMax.set(".oval", { x: -100 });
+
+// repeat to infinity
+// var tlanimation = new TimelineMax({ repeat: -1 });
+
+// var tlanimation = new TimelineMax({ repeat: -1, repeatDelay: 2 });
+var tlanimation = new TimelineMax({ repeat: -1, yoyo: true });
+
+tlanimation.to(".oval", 1, { x: 100 }).to("oval", 1, { x: -100 });
+
 // var tlanimation = new TimelineMax();
 
 // tlanimation
@@ -5,9 +15,9 @@
 //   .to(".square", 0.5, { x: 100, ease: Elastic.easeIn })
 //   .to(".rectangle", 1, { x: 100, ease: Bounce.easeIn });
 
-TweenMax.staggerFrom(
-  ".triangle",
-  1,
-  { x: 200, y: 200, ease: Back.easeOut, opacity: 0 },
-  0.1
-);
+// TweenMax.staggerFrom(
+//   ".triangle",
+//   1,
+//   { x: 200, y: 200, ease: Back.easeOut, opacity: 0 },
+//   0.1
+// );
